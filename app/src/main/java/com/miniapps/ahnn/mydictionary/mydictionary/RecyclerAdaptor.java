70c6623f340bfd518bc9274fc92c2e7a2a26ea58
ahmedkhaled19 <ah.khaled1994@gmail.com>
2017-04-16 17:52:03 +0200

    Database - adaptor

--- app/src/main/java/com/miniapps/ahnn/mydictionary/mydictionary/RecyclerAdaptor.java
+++ app/src/main/java/com/miniapps/ahnn/mydictionary/mydictionary/RecyclerAdaptor.java
@@ -1,15 +1,11 @@
 package com.miniapps.ahnn.mydictionary.mydictionary;
 
 import android.content.Context;
-import android.content.Intent;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.List;
 
